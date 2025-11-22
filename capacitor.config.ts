f3d920370ea4b09f5e501aa42b41cdff76f8ccdc
MatthewSamaha <matthewsamaha@gmail.com>
2025-11-22 13:32:59 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SignalsMaster',
+  webDir: 'www'
+};
+
+export default config;
